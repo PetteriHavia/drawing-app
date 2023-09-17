d47443126ee20aae12a4cf2059b9c3d69b397504
Petteri Havia <petsku15@hotmail.fi>
2023-09-18 00:17:45 +0300

    Testing line-color state with redux/toolkit

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,7 +1,20 @@
+import { useContext } from "react";
 import { Container, InnerContainer, ToolBar, ToolSettings } from "../styles/Elements.style";
 import {BiCircle, BiSquare} from 'react-icons/bi'
+import { useDispatch, useSelector } from "react-redux";
+import { changeColor} from "../redux/LineReducer"; 
 
 const Menu = () => {
+
+  const { color } = useSelector((state) => state.color);
+  const dispatch = useDispatch();
+  const lineColor = useSelector((state) => state.color.lineColor);
+
+  const handleChangeColor = () => {
+
+  }
+
+
   return (
     <Container>
       <InnerContainer>
@@ -10,7 +23,7 @@ const Menu = () => {
           <input id="line-width" defaultValue="1" type="number" min="1" max="100"/>
         </ToolSettings>
         <ToolBar>
-          <input type="color" defaultValue="#000000" />
+          <input type="color" defaultValue={lineColor} />
           <BiSquare />
           <BiCircle />
         </ToolBar>
