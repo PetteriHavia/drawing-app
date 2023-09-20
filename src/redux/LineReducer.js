1392008c281d4f378e266ef087476687e2996e92
Petteri Havia <56974048+PetteriHavia@users.noreply.github.com>
2023-09-20 06:18:47 +0300

    States for width, color and opacity

--- src/redux/LineReducer.js
+++ src/redux/LineReducer.js
@@ -1,17 +1,47 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-export const lineSlice = createSlice({
+export const colorSlice = createSlice({
   name: "LineColor",
   initialState: {
     lineColor: "#a05858",
   },
   reducers: {
     changeColor: (state, action) => {
-      state.color = action.payload;
+      state.lineColor = action.payload;
     },
   },
 });
 
-export const { changeColor } = lineSlice.actions;
 
-export const colorReducer = lineSlice.reducer;
+export const opacitySlice = createSlice({
+  name: "LineOpacity",
+  initialState: {
+    lineOpacity: "1",
+  },
+  reducers: {
+    changeOpacity: (state, action) => {
+      state.lineOpacity = action.payload;
+    },
+  },
+});
+
+export const widthSlice = createSlice({
+  name: "LineOpacity",
+  initialState: {
+    lineWidth: "5",
+  },
+  reducers: {
+    changeWidth: (state, action) => {
+      state.lineWidth = action.payload;
+    },
+  },
+});
+
+export const { changeColor } = colorSlice.actions;
+export const { changeOpacity } = opacitySlice.actions;
+export const { changeWidth } = widthSlice.actions;
+
+export const colorReducer = colorSlice.reducer;
+export const opacityReducer = opacitySlice.reducer;
+export const widthReducer = widthSlice.reducer;
+
