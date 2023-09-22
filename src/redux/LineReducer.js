940660e2da10bfe24b8bfa89068507d6776aac15
Petteri Havia <56974048+PetteriHavia@users.noreply.github.com>
2023-09-22 04:52:15 +0300

    Add state and reducers for color history and palette

--- src/redux/LineReducer.js
+++ src/redux/LineReducer.js
@@ -12,12 +12,45 @@ export const colorSlice = createSlice({
   },
 });
 
-//Working on it
 export const colorHistorySlice = createSlice({
   name: "ColorHistory",
-
+  initialState: {
+    colorHistory: [],
+  },
+  reducers: {
+    addColor: (state, action) => {
+      if (state.colorHistory.length >= 10) {
+        state.colorHistory.shift();
+        state.colorHistory.push(action.payload);
+      } else {
+        state.colorHistory.push(action.payload);
+      }
+    },
+  },
 });
 
+export const colorPaletteSlice = createSlice({
+  name: "ColorPalette",
+  initialState: {
+    colorPalette: [
+      {color: "#FCBA04"},
+      {color: "#E9080B"},
+      {color: "#c922c9"},
+      {color: "#3b1bb1"},
+      {color: "#7c358a"},
+      {color: "#831b23"},
+      {color: "#b3a789"},
+      {color: "#3ca753"},
+      {color: "#3be9e0"},
+      {color: "#4be4be"},
+    ]
+  },
+   reducers: {
+    updatePalette: (state, action) => {
+
+    } 
+   },
+});
 
 export const opacitySlice = createSlice({
   name: "LineOpacity",
@@ -46,8 +79,11 @@ export const widthSlice = createSlice({
 export const { changeColor } = colorSlice.actions;
 export const { changeOpacity } = opacitySlice.actions;
 export const { changeWidth } = widthSlice.actions;
+export const { addColor, removeColor } = colorHistorySlice.actions;
+export const { updatePalette} = colorPaletteSlice.actions;
 
 export const colorReducer = colorSlice.reducer;
 export const opacityReducer = opacitySlice.reducer;
 export const widthReducer = widthSlice.reducer;
-
+export const colorHistoryReducer = colorHistorySlice.reducer;
+export const colorPaletteReducer = colorPaletteSlice.reducer;
