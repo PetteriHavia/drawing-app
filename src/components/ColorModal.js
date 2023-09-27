bd3f119fbea76e8bb717929d6af5e11ff88b57ab
Petteri Havia <petsku15@hotmail.fi>
2023-09-27 07:22:30 +0300

    Setup color palette component that let's user to select 10 different colors. At the moment the colorModal works, but is not so user friendly

--- src/components/ColorModal.js
+++ src/components/ColorModal.js
@@ -1,29 +1,45 @@
 import { useDispatch, useSelector } from "react-redux";
-import {
-  ConfigureModal,
-  Inner,
-} from "../styles/Elements.style";
+import { ConfigureModal, Inner } from "../styles/Elements.style";
 import { FiX } from "react-icons/fi";
+import { changeColor, updatePalette } from "../redux/LineReducer";
+import { colorModalHide } from "../redux/ModalReducer";
 
 const ColorModal = () => {
   const { colorPalette } = useSelector((state) => state.palette);
-  const dispatch = useDispatch;
+  const { colorModal } = useSelector((state) => state.colorModal);
+  const dispatch = useDispatch();
 
-  const MapColors = ({ item }) => {
-    return <input type="color" defaultValue={item.color} />;
+  const MapColors = ({ item, index }) => {
+    return (
+      <input
+        type="color"
+        value={item.color}
+        onChange={(e) => configureColors(e, index)}
+      />
+    );
+  };
+
+  const configureColors = (e, index) => {
+    
+    dispatch(updatePalette({ index, color: e.target.value }));
+    dispatch(changeColor(e.target.value));
+  };
+
+  const handleHideModal = () => {
+    dispatch(colorModalHide());
   };
 
   return (
     <ConfigureModal>
       <div className="configure-modal-header">
-      	<h3>Configure Color Palette</h3>
-      	<FiX />
+        <h3>Configure Color Palette</h3>
+        <FiX onClick={handleHideModal}/>
       </div>
       <Inner>
-        {colorPalette.map((item) => (
-          <MapColors key={item.color} item={item} />
+        {colorPalette.map((item, index) => (
+          <MapColors key={index} item={item} index={index} />
         ))}
-        <button>Save</button>
+        <button onClick={handleHideModal}>OK</button>
       </Inner>
     </ConfigureModal>
   );
