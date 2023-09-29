9651adfd1bf0ab6ccb541ac6f19247497fad6f9c
Petteri Havia <petsku15@hotmail.fi>
2023-09-29 06:42:16 +0300

    Update

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,22 @@
 import Menu from "../components/Menu";
 import Canvas from "../components/Canvas";
 import SideMenu from "../components/SideMenu";
-import {Grid } from "../styles/Elements.style"
+import { Grid } from "../styles/Elements.style";
 import { useRef } from "react";
 
 const Home = () => {
 
-    const ctxRef = useRef(null);
-
-    return(
-        <>
-        <Menu ctxRef={ctxRef}/>
-        <Grid>
-            <SideMenu />
-            <Canvas ctxRef={ctxRef}/>
-        </Grid>
-        </>
-    )
-}
+  const ctxRef = useRef(null);
+  
+  return (
+    <>
+      <Menu ctxRef={ctxRef} />
+      <Grid>
+        <SideMenu ctxRef={ctxRef} />
+        <Canvas ctxRef={ctxRef} />
+      </Grid>
+    </>
+  );
+};
 
 export default Home;
