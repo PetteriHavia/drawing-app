e0b33b6075f01feb72ad2f4adb91266158768ec0
Petteri Havia <56974048+PetteriHavia@users.noreply.github.com>
2023-09-29 06:36:45 +0300

    Relocated undo and clear canvas buttons to SideMenu. Add new redo function

--- src/components/SideMenu.js
+++ src/components/SideMenu.js
@@ -1,17 +1,46 @@
 import { changeColor } from "../redux/LineReducer";
 import { useDispatch, useSelector } from "react-redux";
-import { BiCircle, BiSquare, BiSolidTrash } from "react-icons/bi";
-import {
-  ToolBar,
-  SideBar,
-} from "../styles/Elements.style";
+import { BiEraser, BiTrash, BiPencil, BiUndo, BiRedo } from "react-icons/bi";
+import { ToolBar, SideBar } from "../styles/Elements.style";
+import { addToHistory, addToredoHistory, changeTool, deleteFromHistory, deleteFromredoHistory, redoReducer } from "../redux/DrawingReducer";
+import {modalActive } from "../redux/ModalReducer";
 
 const SideMenu = () => {
+
   const { lineColor } = useSelector((state) => state.color);
+  const {lineHistory} = useSelector((state) => state.history);
+  const { redoHistory } = useSelector((state) => state.redo);
   const dispatch = useDispatch();
   
   const handleChangeColor = (e) => {
     dispatch(changeColor(e.target.value));
+  };
+
+  const handleToolChange = (selectedTool) => {
+    dispatch(changeTool(selectedTool))
+  }
+
+  const handleShowModal = () => {
+    if (lineHistory.length > 0) {
+      dispatch(modalActive());
+    }
+  };
+
+  const handleDelete = () => {
+    if (lineHistory.length > 0) {
+      const saveRedo = lineHistory.slice(-1)[0];
+      dispatch(addToredoHistory(saveRedo));
+      dispatch(deleteFromHistory());
+    }
+  };
+
+  const handleRedo = () => {
+    if(redoHistory.length > 0) {
+      const addBack = redoHistory.slice(-1)[0];
+      console.log(addBack);
+      dispatch(addToHistory(addBack));
+      dispatch(deleteFromredoHistory()); 
+    }
   };
 
   return (
@@ -22,11 +51,11 @@ const SideMenu = () => {
             value={lineColor}
             onChange={handleChangeColor}
           />
-          <BiSquare />
-          <BiCircle />
-          <div>
-          <BiSolidTrash />
-          </div>
+          <button onClick={() => handleToolChange("pencil")}><BiPencil/></button>
+          <button onClick={() => handleToolChange("eraser")}><BiEraser/></button>
+          <button onClick={handleDelete}><BiUndo/></button>
+          <button onClick={handleRedo}><BiRedo/></button>
+          <button onClick={handleShowModal}><BiTrash/></button>
         </ToolBar>
     </SideBar>
   );
